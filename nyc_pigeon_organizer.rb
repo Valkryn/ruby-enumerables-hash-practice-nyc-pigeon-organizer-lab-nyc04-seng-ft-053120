da5dbf430e91c0022611f5057469c0b1db788bdb
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-06-07 18:38:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = data.each_with_object({}) do | (key, value) , final_a |
+    value.each do | inner_k , inner_v |
+      inner_v.each do | name |
+        if !final_a[name]
+          final_a[name] = {}
+        end
+        if !final_a[name][key]
+          final_a[name][key] =[]
+        end
+        final_a[name][key] << inner_k.to_s
+      end
+    end
+  end
+  new_data
 end
